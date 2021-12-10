832ca1169baf1b87137593ddf3cdd0fe4d2ac3e2
XFO28I <jav.nandor@gmail.com>
2021-12-10 18:19:03 +0100

    Initial

---
+++ Beadandó_7/Beadandó_7/Entities/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beadandó_7.Entities
+{
+    class Ball: Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = Height = 50;
+            Paint += Ball_Paint;
+        }
+            private void Ball_Paint(object sender, PaintEventArgs e)
+            {
+                DrawImage(e.Graphics);
+            }
+
+        void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+    }
+}
