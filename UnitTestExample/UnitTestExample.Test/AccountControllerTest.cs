0a409c39a0ff833a3419a04b50f180eee0e04d81
XFO28I <jav.nandor@gmail.com>
2021-12-19 19:50:56 +0100

    done

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTest.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTest
+    {
+        [Test,
+        TestCase("abcd1234", false),
+        TestCase("irf@uni-corvinus", false),
+        TestCase("irf.uni-corvinus.hu", false),
+        TestCase("irf@uni-corvinus.hu", true)]
+
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            //Arrange (logikai blokkok)
+            var accountController = new AccountController();
+            //Act
+            var actualResult = accountController.ValidateEmail(email);
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+        [Test,
+            TestCase("Abc1", false),
+            TestCase("Abcdefgh", false),
+            TestCase("ABCDEFGH", false),
+            TestCase("Abc1Abc1", true) 
+        ]
+        public void TestValidatePassword (string password, bool expectedResult)
+        {
+            //Arrange (logikai blokkok)
+            var accountController = new AccountController();
+            //Act
+            var actualResult = accountController.ValidatePassword(password);
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test,
+            TestCase("irf@uni-corvinus.hu","Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234567")
+        ]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            //Arrange (logikai blokkok)
+            var accountController = new AccountController();
+            //Act
+            var actualResult = accountController.Register(email, password);
+            //Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+        [Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "aBcd1234"),
+            TestCase("irf@uni-corvinus.hu", "abCd1234")
+        ]
+
+        public void TestRegisterValidateException(string email, string password)
+        {
+            //Arrange (logikai blokkok)
+            var accountController = new AccountController();
+            //Act
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+            //Assert
+            
+
+        }
+    }
+}
