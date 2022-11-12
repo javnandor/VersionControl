a5d41f5add68997fc10ce0aeb6764c7831432e9a
Nándor Jávorszki <nandor.javorszki@stud.uni-corvinus.hu>
2022-11-12 09:23:43 +0100

    Feladat1

---
+++ IRF_Harmadik_LINQ/IRF_Harmadik_LINQ/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Harmadik_LINQ
+{
+    class Country
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+
+    }
+
+}
