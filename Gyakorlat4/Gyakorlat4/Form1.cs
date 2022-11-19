3d2c7af2a66d11f745ad8f814621bd8963ec5b05
Nándor Jávorszki <nandor.javorszki@stud.uni-corvinus.hu>
2022-11-19 17:16:21 +0100

    Feladat4

--- Gyakorlat4/Gyakorlat4/Form1.cs
+++ Gyakorlat4/Gyakorlat4/Form1.cs
@@ -12,9 +12,18 @@ namespace Gyakorlat4
 {
     public partial class Form1 : Form
     {
+
+        List<Flat> flats;
+        RealEstateEntities re = new RealEstateEntities();
+        void LoadData()
+        {
+            flats = re.Flats.ToList();
+        }
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
     }
 }
