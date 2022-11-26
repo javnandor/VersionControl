6d671c17c29b15875194b6e5a5973a05717d430b
Nándor Jávorszki <nandor.javorszki@stud.uni-corvinus.hu>
2022-11-26 09:16:18 +0100

    feladat3

--- Gyakorlat05/Gyakorlat05/Form1.cs
+++ Gyakorlat05/Gyakorlat05/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gyakorlat05.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,18 @@ namespace Gyakorlat05
         public Form1()
         {
             InitializeComponent();
+            
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            GetExchangeRatesResponseBody response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            MessageBox.Show(result);
+
         }
     }
 }
