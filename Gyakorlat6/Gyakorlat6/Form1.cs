9271c4568782f55c46ac6dadef41c46719fac192
Nándor Jávorszki <nandor.javorszki@stud.uni-corvinus.hu>
2022-12-17 13:32:26 +0100

    feladat4

--- Gyakorlat6/Gyakorlat6/Form1.cs
+++ Gyakorlat6/Gyakorlat6/Form1.cs
@@ -12,6 +12,9 @@ namespace Gyakorlat6
 {
     public partial class Form1 : Form
     {
+        List<Tick> ticks;
+        PortfolioEntities context = new PortfolioEntities();
+       
         public Form1()
         {
             InitializeComponent();
