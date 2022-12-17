de10b6d2ff77a78c1c091236fe08714aa8ff108c
Nándor Jávorszki <nandor.javorszki@stud.uni-corvinus.hu>
2022-12-17 13:55:03 +0100

    Feladat5

--- Gyakorlat6/Gyakorlat6/Form1.cs
+++ Gyakorlat6/Gyakorlat6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gyakorlat6.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,12 +16,25 @@ namespace Gyakorlat6
         
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
 
         public Form1()
         {
             InitializeComponent();
             ticks = context.Tick.ToList();
             dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+
 
         }
     }
